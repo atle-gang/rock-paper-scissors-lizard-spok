1e671b1faa155c99e4fa2fad6e7fef659d47caa5
Atlegang Sethono <atlegangcode@gmail.com>
2023-08-10 11:11:59 +0200

    Create checkWinner function that will decide winner

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function getComputerChoice(){
     const computerChoice = weaponOptions[Math.floor(Math.random() * weaponOptions.length)];
     return computerChoice
 };
+
+
+// create function that checks the winner based on their selections
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Tie'
+    } else if (
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'rock' && computerSelection === 'lizard') ||
+        (playerSelection === 'lizard' && computerSelection === 'spok') ||
+        (playerSelection === 'rock' && computerSelection === 'lizard') ||
+        (playerSelection === 'spok' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'lizard') ||
+        (playerSelection === 'lizard' && computerSelection === 'paper') ||
+        (playerSelection === 'spok' && computerSelection === 'rock')
+    ) {
+        return 'Player'
+    } else {
+        return 'Computer'
+    }
+};
