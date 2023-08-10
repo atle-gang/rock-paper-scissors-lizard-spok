254f2675b73967f4b26cfb012d45d555b1c5d3ca
Atlegang Sethono <atlegangcode@gmail.com>
2023-08-10 15:21:10 +0200

    Refractor code to improve readability

--- script.js
+++ script.js
@@ -1,93 +1,122 @@
 const weaponOptions = ['rock', 'paper', 'scissors', 'lizard', 'spok'];
 
-// create function that will randomise weaponOptions everytime function is called
+// Generate a random weapon choice each time the function is called.
 function getComputerChoice(){
     const computerChoice = weaponOptions[Math.floor(Math.random() * weaponOptions.length)];
     return computerChoice
 };
 
 
-// create function that checks the winner based on their selections
+// Define object for winning combinations.
+const winningCombinations = {
+    rock: ['scissors', 'lizard'],
+    paper: ['rock', 'spok'],
+    scissors: ['paper', 'lizard'],
+    lizard: ['paper', 'spok'],
+    spok: ['rock', 'scissors']
+};
+
+
+// Define constant object for possible outcomes of the game.
+const GAME_OUTCOME = {
+    TIE: 'Tie',
+    PLAYER_WIN: 'Player',
+    COMPUTER_WIN: 'Computer'
+};
+
+
+// Determine the winner based on player and computer selections.
 function checkWinner(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return 'Tie'
-    } else if (
-        (playerSelection === 'scissors' && computerSelection === 'paper') ||
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'rock' && computerSelection === 'lizard') ||
-        (playerSelection === 'lizard' && computerSelection === 'spok') ||
-        (playerSelection === 'rock' && computerSelection === 'lizard') ||
-        (playerSelection === 'spok' && computerSelection === 'scissors') ||
-        (playerSelection === 'scissors' && computerSelection === 'lizard') ||
-        (playerSelection === 'lizard' && computerSelection === 'paper') ||
-        (playerSelection === 'spok' && computerSelection === 'rock')
-    ) {
-        return 'Player'
+        return GAME_OUTCOME.TIE;
+    } else if (winningCombinations[playerSelection].includes(computerSelection)) {
+        return GAME_OUTCOME.PLAYER_WIN;
     } else {
-        return 'Computer'
+        return GAME_OUTCOME.COMPUTER_WIN;
     }
 };
 
 
-// create a function that gets and validates user input
+// Get and validate the user's weapon choice.
 function getPlayerChoice() {
-    let validatedInput = false;
-    while (validatedInput === false){
+    let isValidInput = false;
+    while (isValidInput === false) {
         const playerChoice = prompt("Select your weapon: ");
         if (playerChoice === null) {
             continue;
         } else {
             const playerChoiceInLower = playerChoice.toLowerCase();
             if (weaponOptions.includes(playerChoiceInLower)) {
-                validatedInput = true;
+                isValidInput = true;
                 return playerChoiceInLower;
             };
-        };
+        }
     };
 };
 
-// create a function that simulates a single round of the game
+
+// Create a function that simulates a single round of the game.
 function playRound(playerSelection, computerSelection) {
     const result = checkWinner(playerSelection, computerSelection)
     if (result === 'Tie') {
         return "It's a tie!"
     } else if (result === 'Player') {
-        return `You wins! ${playerSelection} beats ${computerSelection}.`
+        return `You win! ${playerSelection} beats ${computerSelection}.`
     } else {
         return `You lose! ${computerSelection} beats ${playerSelection}.`
     }
 };
 
 
-// create game function that'll play 5 rounds of the game
-function game(){
-    // intialise computer and player scores
-    let playerScore = 0;
-    let computerScore = 0;
-
-    // Loop that'll allow 5 rounds of the game to be played
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice(); 
-        
-        console.log(playRound(playerSelection, computerSelection))
-        
-        if (checkWinner(playerSelection, computerSelection) === 'Player') {
-            playerScore++
-        } else if (checkWinner(playerSelection, computerSelection) === 'Computer'){
-            computerScore++
-        };
-    };
-    if (playerScore > computerScore) {
-        console.log('You win! Congratulations.')
-    } else if (computerScore > playerScore) {
-        console.log("You lose! You got beaten by the computer")
-    } else {
-        console.log("We have a tie! Play another game round to settle the tie")
+// Intialize player and computer scores.
+function initliaseScores() {
+    return {
+        player: 0,
+        computer: 0
     };
 };
 
-// UI implementation
+
+// Function to loop through game.
+function playGameRounds(numberOfRounds) {
+    let scores = initliaseScores();
+
+    for (let i = 0; i < numberOfRounds; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        const winner = checkWinner(playerSelection, computerSelection); 
+        
+        console.log(playRound(playerSelection, computerSelection));
+        
+        if (winner === 'Player') {
+            playerScore++;
+        } else if (winner === 'Computer') {
+            computerScore++;
+        }
+    };
+
+    return scores;
+};
+
+
+// Display the final outcome of the game based on player and computer scores.
+function displayFinalOutcome(scores) {
+    if (scores.player > scores.computer) {
+        console.log('Game Over. You win! Congratulations.');
+    } else if (scores.computer > scores.player) {
+        console.log("Game Over. You lose! You got beaten by the computer.");
+    } else {
+        console.log("Game Over. We have a tie! Play another game round to settle the tie.");
+    }
+};
+
+
+// Function to play 5 rounds of the game.
+function game(){
+    const numberOfRounds = 5;
+    const scores = playGameRounds(numberOfRounds);
+    displayFinalOutcome(scores);
+};
+
 
 game()
